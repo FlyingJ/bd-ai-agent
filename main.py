8f63cb85a3e174d089272a6e253093b731dff6f3
FlyingJ <jason.m.harrington@gmail.com>
2025-07-15 14:57:58 -0400

    Add verbose flag

--- main.py
+++ main.py
@@ -15,10 +15,20 @@ def main():
 
     model = "gemini-2.0-flash-001"
     # contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    user_prompt = ' '.join(sys.argv[1:])
-    if len(user_prompt) < 3:
+    
+    verbose = False
+    if sys.argv[-1] == "--verbose":
+        verbose = True
+        #print(f'{sys.argv[0]}: verbose output set')
+        # FIXME
+        # remove the verbose arg to keep user_prompt happy
+        sys.argv.pop()
+
+    if len(sys.argv) != 2:
         print("Error: no prompt provided")
         sys.exit(1)
+
+    user_prompt = sys.argv.pop()
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
@@ -29,13 +39,16 @@ def main():
         contents=messages,
     )
 
-    print(f"""
-        Model: {model}
-        Prompt: {contents}
-        Response: {response.text}
-        Prompt tokens: {response.usage_metadata.prompt_token_count}
-        Response tokens: {response.usage_metadata.candidates_token_count}
-        """)
+    print(f"    Model: {model}")
+
+    if verbose:
+        print(f"    User prompt: {user_prompt}")
+
+    print(f"    Response: {response.text.strip()}")
+
+    if verbose:
+        print(f"    Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"    Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
