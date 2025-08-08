4e5c574891df59dde5721bd8d79a4ad9ab49870a
FlyingJ <jason.m.harrington@gmail.com>
2025-08-07 22:57:32 -0400

    Add functions.get_files_info() and supporting code

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,19 +6,50 @@ def get_files_info(working_directory, directory="."):
 	wd = Path(working_directory)
 	td = wd / directory
 
-	# validate absolute_directory lies within absolute_working_directory or throw exception
-	if wd not in td.parents:
-		return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-	# validate absolute_directory is a directory
-	if not td.is_dir():
-		return f'Error: "{directory}" is not a directory'
+	def validate_is_jailed(wd, td):
+		if wd in td.parents:
+			return True
+		else:
+			raise Exception(f'Error: Cannot list "{td}" as it is outside the permitted working directory')
 
+	def validate_is_dir(td):
+		if td.is_dir():
+			return True
+		else:
+			raise Exception(f'Error: "{td}" is not a directory')
+
+	try:
+		# validate absolute_directory lies within absolute_working_directory or throw exception
+		validate_is_jailed(wd, td) 
+		# validate absolute_directory is a directory
+		validate_is_dir(td)
+	except Exception as e:
+		return e
+
+	# FIX: this "redefinition" seems unnecessary
 	directory = Path(directory)
 
 	response = ''
-	for filename in directory.glob('*'):
+	
+	try:
+		filenames = directory.glob('*')
+	except Exception as e:
+		return f"Error: {e}"
+
+	for filename in filenames:
 		path = directory / filename
-		path_info_string = f'- {filename}: file_size={path.stat().st_size} bytes, is_dir={path.is_dir()}\n'
+		
+		try:
+			file_size = path.stat().st_size
+		except Exception as e:
+			return f"Error: {e}"
+
+		try:
+			file_is_dir = path.is_dir()
+		except Exception as e:
+			return f"Error: {e}"
+
+		path_info_string = f'- {filename}: file_size={file_size} bytes, is_dir={file_is_dir}\n'
 		response += path_info_string
 
 	return response
