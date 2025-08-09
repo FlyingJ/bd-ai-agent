511833feee741ac2d7df70e7c491c1c2f8967615
FlyingJ <jason.m.harrington@gmail.com>
2025-08-09 00:21:26 -0400

    Add functions.get_files_info() and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,55 +1,90 @@
+import glob
 import os
+import unittest
 
-from pathlib import Path
+def validate_is_jailed(a, b):
+	'''
+	Return True, if the absolute path of b is a sub-directory of a;
+	otherwise, raise Exception
+
+	Args:
+		a (str): containing directory path
+		b (str): contained directory path (relative path to a)
+
+	Returns:
+		True if b contained within a,
+		or raise Exception with apropos message
+	'''
+	if os.path.commonpath([
+		os.path.abspath(os.path.normpath(a)),
+		os.path.abspath(os.path.normpath(os.path.join(a,b))),
+		]) == os.path.abspath(os.path.normpath(a)):
+		return True
+	else:
+		raise Exception(f'Cannot list "{b}" as it is outside the permitted working directory')
+
+def validate_is_dir(a):
+	'''
+	Return True, if path a is a directory;
+	otherwise, raise Exception
+
+	Args:
+		a (str): unvalidated directory path
+
+	Returns:
+		True a is a directory path,
+		or raise Exception with apropos message
+	'''
+	if os.path.isdir(a):
+		return True
+	else:
+		raise Exception(f'"{a}" is not a directory')
 
 def get_files_info(working_directory, directory="."):
-	wd = Path(working_directory)
-	td = wd / directory
+	norm_working_path = os.path.normpath(working_directory)
+	norm_directory_path = os.path.normpath(
+		os.path.join(working_directory, directory)
+		)
 
-	def validate_is_jailed(wd, td):
-		if wd in td.parents:
-			return True
-		else:
-			raise Exception(f'Error: Cannot list "{td}" as it is outside the permitted working directory')
+	#	Result for current directory: -> directory = '.'
+	#	Result for '{directory}' directory: -> directory is anything else
+	if norm_working_path == norm_directory_path:
+		directory_designation = "current"
+	else:
+		directory_designation = f"'{directory}'"
 
-	def validate_is_dir(td):
-		if td.is_dir():
-			return True
-		else:
-			raise Exception(f'Error: "{td}" is not a directory')
-
-	try:
-		# validate absolute_directory lies within absolute_working_directory or throw exception
-		validate_is_jailed(wd, td) 
-		# validate absolute_directory is a directory
-		validate_is_dir(td)
-	except Exception as e:
-		return e
-
-	# FIX: this "redefinition" seems unnecessary
-	directory = Path(directory)
-
-	response = ''
+	response = f"Result for {directory_designation} directory:\n"
 	
+	# beware symbolic links and other path trickery
 	try:
-		filenames = directory.glob('*')
+		# validate working_directory / directory lies within working_directory
+		# or throw exception
+		validate_is_jailed(working_directory, directory) 
+		# validate norm_directory_path (directory we are listing) is a directory
+		validate_is_dir(norm_directory_path)
 	except Exception as e:
-		return f"Error: {e}"
+		return response + f"    Error: {e}\n"
+
+	try:
+		filenames = glob.glob(
+			"*",
+			root_dir=norm_directory_path
+			)
+	except Exception as e:
+		return response + f"    Error: {e}\n"
 
 	for filename in filenames:
-		path = directory / filename
-		
-		try:
-			file_size = path.stat().st_size
-		except Exception as e:
-			return f"Error: {e}"
+		filepath = os.path.normpath(
+			os.path.join(working_directory, directory, filename)
+			)
 
 		try:
-			file_is_dir = path.is_dir()
+			file_size = os.stat(filepath).st_size
+			file_is_dir = os.path.isdir(filepath)
 		except Exception as e:
-			return f"Error: {e}"
+	 		return response + f"    Error: {e}\n"
 
-		path_info_string = f'- {filename}: file_size={file_size} bytes, is_dir={file_is_dir}\n'
+		path_info_string = f' - {filename}: file_size={file_size} bytes, is_dir={file_is_dir}\n'
 		response += path_info_string
 
 	return response
