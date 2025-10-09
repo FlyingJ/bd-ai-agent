c6c3bd4ede6878621d548b218d0c6ea3dd9089bf
FlyingJ <jason.m.harrington@gmail.com>
2025-10-08 23:34:22 -0400

    Complete functions.run_python_file() and add apropos tests to tests.py

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -3,6 +3,8 @@ import subprocess
 
 from functions.validation import validate_is_jailed
 
+COMMAND_TIMEOUT=30
+
 def run_python_file(working_directory, file_path, args=[]):
 	norm_working_path = os.path.normpath(working_directory)
 	norm_file_path = os.path.normpath(
@@ -19,3 +21,26 @@ def run_python_file(working_directory, file_path, args=[]):
 		# ensure file_path has .py extension (no guarantee of code, but)
 		if not file_path.endswith('.py'):
 			raise Exception(f'Error: "{file_path}" is not a Python file.')
+	except Exception as e:
+		print(f'Error: {e}')
+
+	try:
+		result = subprocess.run(
+			['python3', file_path],
+			*args,
+			capture_output=True,
+			cwd=norm_working_path,
+			timeout=COMMAND_TIMEOUT,
+			)
+	except Exception as e:
+		return (f'Error: executing Python file: {e}')
+
+	return_string = ''
+	return_string += f'STDOUT: {result.stdout}\n'
+	return_string += f'STDERR: {result.stderr}\n'
+	if result.returncode != 0:
+		return_string += f'Process exited with code {result.returncode}\n'
+	if len(result.stderr) == 0:
+		return_string += f'No output produced\n'
+
+	return return_string
