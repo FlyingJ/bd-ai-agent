fcfe6b47ea3a76351e319d0b07cdd0585a2b0bb8
FlyingJ <jason.m.harrington@gmail.com>
2025-10-14 15:39:59 -0400

    More refactor, add basic feedback loop

--- functions/config.py
+++ functions/config.py
@@ -1,4 +1,23 @@
 # limit command execution time in functions/run_python_file()
 COMMAND_TIMEOUT=30
 # limit number of characters read in when sending to AI backend
-MAX_CHARS = 10000
+MAX_CHARS=10000
+# limit the number of times we will allow the agent to iterate
+MAX_AGENT_ITERATIONS=20
+
+# system prompt to let the agent know what to do
+SYSTEM_PROMPT = '''
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan.
+
+The following operations are available:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+Provide paths relative to the working directory.
+Do not specify the working directory in function calls as it is hard-coded for security reasons.
+'''
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.config import MAX_AGENT_ITERATIONS, SYSTEM_PROMPT
+
 from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.run_python_file import run_python_file, schema_run_python_file
@@ -60,32 +62,21 @@ def call_function(function_call_part, verbose=False):
         ]
     )
 
-SYSTEM_PROMPT = '''
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan.
-
-The following operations are available:
-
-- List files and directories
-- Read file contents
-- Execute Python files with optional arguments
-- Write or overwrite files
-
-Provide paths relative to the working directory.
-Do not specify the working directory in function calls as it is hard-coded for security reasons.
-'''
-
 def main():
     print("Hello from bd-ai-agent!")
 
+    # read in API key via environment variable
+    # the client will use the key to make calls to the AI API
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    # specify the model to use
     model = "gemini-2.0-flash-001"
     print(f"    Model: {model}")
 
+    # this seems a janky way of dealing with verbosity and arguments in general
+    # perhaps the user prompt should be read in from a file or command line
     verbose = False
     if sys.argv[-1] == "--verbose":
         verbose = True
@@ -107,29 +98,43 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    response = client.models.generate_content(
-        model=model,
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools = [available_functions],
-            system_instruction=SYSTEM_PROMPT
-        ),
-    )
+    agent_iterations = 0
+    while agent_iterations < MAX_AGENT_ITERATIONS:
+        agent_iterations += 1
 
-    if response.function_calls:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, verbose=True)
-            if not function_call_result.parts[0].function_response.response:
-                raise Exception(f'    Error: failed to call function: "{function_call.name}"')
-            elif verbose:
-                print(f' -> {function_call_result.parts[0].function_response.response}')
-    
-    if response.text:
-        print(f"    Response: {response.text.strip()}")
+        # send updated messages to model and save response
+        response = client.models.generate_content(
+            model=model,
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools = [available_functions],
+                system_instruction=SYSTEM_PROMPT
+            ),
+        )
 
-    if verbose:
-        print(f"    Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"    Response tokens: {response.usage_metadata.candidates_token_count}")
+        # if response makes function calls
+        # call the functions
+        # Q: update messages with response?
+        if response.function_calls:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, verbose=True)
+                if not function_call_result.parts[0].function_response.response:
+                    raise Exception(f'    Error: failed to call function: "{function_call.name}"')
+                elif verbose:
+                    print(f' -> {function_call_result.parts[0].function_response.response}')
+
+        # still doing stuff
+        # update messages and iterate
+        
+        # if no new function call and only text response, we are done
+        # jump out of the loop
+        if response.text:
+            print(f"    Response: {response.text.strip()}")
+            break
+
+        # if verbose:
+        #     print(f"    Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        #     print(f"    Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
